42b6b32392217f8c78537d96be2aa7731a11c8a1
Josh Pyles <josh@pixelmatrixdesign.com>
2010-03-08 18:18:40 -0800

    Added direction option and support for multiple keycodes per shortcut

--- jquery.mapkey.js
+++ jquery.mapkey.js
@@ -1,6 +1,6 @@
 /*
 
-Map Key v0.6
+Map Key v0.7
 Copyright © 2010 Josh Pyles / Pixelmatrix Design LLC
 http://pixelmatrixdesign.com
 
@@ -19,19 +19,20 @@ $.mapKey("left", function(){
   alert("you hit the left arrow!");
 });
 
+Parameters:
+trigger: (function) - trigger a jQuery event
+direction: "up"/"down" - use keydown or keyup event 
+
 Enjoy!
 
 */
 
 (function($) {
-  $.mapKey = function(key, newfunc){
-    var code;
-    if(typeof key == "string"){
-      code = $.fn.mapKey.keys[key];
-    }else if(typeof key == "integer"){
-      code = key;
-    }
-    $.fn.mapKey.bindings[code.toString()] = newfunc;
+  $.mapKey = function(key, newfunc, options){
+    //grab the opts
+    var o = $.extend({}, $.fn.mapKey.defaults, options);
+    //store a binding
+    $.fn.mapKey.storeBinding(key, newfunc, o.direction);
   }
   $.fn.mapKey = function(key, options) {
     //debug(this);
@@ -43,58 +44,121 @@ Enjoy!
       // build element specific options
       var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
       var t = "";
+      var action;
       
-      var code;
-      if(typeof key == "string"){
-        code = $.fn.mapKey.keys[key]; 
-      }else if(typeof key == "integer"){
-        code = key;
-      }
-      
+      //check for a trigger
       if (o.trigger) {
-        $.fn.mapKey.bindings[code.toString()] = function() { $this.trigger(o.trigger); };
-      } else if($this.is("a[href]")){
-        $.fn.mapKey.bindings[code.toString()] = function() { window.location = $this.attr("href") };
+        action = function() { $this.trigger(o.trigger); };
+      }else if($this.is("a[href]")){
+        action = function() { window.location = $this.attr("href"); };
       }
+      
+      //store a binding
+      $.fn.mapKey.storeBinding(key, action, o.direction);
+
     });
   };
   //
   // define and expose our functions
   //
-  $.fn.mapKey.kdown = function(e){
-    //figure out the code of the key pressed
-    var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-    
-    if($.fn.mapKey.bindings[key] != undefined){
-      e.preventDefault();
+  $.fn.mapKey.storeBinding = function(key, value, e){
+    if(typeof key == "number"){
+      //we have all we need already!
+      if(e == "up"){
+        $.fn.mapKey.bindings.up[key] = value;
+      }else{
+        $.fn.mapKey.bindings.down[key] = value;
+      }
+    }else{
+      //get keycode from key database
+      var code = $.fn.mapKey.keys[key];
+      //see if we need to store one or more bindings
+      if(typeof code == "number"){
+        //add a single binding
+        if(e == "up"){
+          $.fn.mapKey.bindings.up[code.toString()] = value;
+        }else{
+          $.fn.mapKey.bindings.down[code.toString()] = value;
+        }
+      }else if(typeof code == "object"){
+        //add multiple bindings for each version of the key
+        $.each(code, function(i, v){
+          if(e == "up"){
+            $.fn.mapKey.bindings.up[v.toString()] = value;
+          }else{
+            $.fn.mapKey.bindings.down[v.toString()] = value;
+          }
+        });
+      } 
     }
   }
-  $.fn.mapKey.kpress = function(e){
-    //figure out the code of the key pressed
-    var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
-    
-    var value = $.fn.mapKey.bindings[key];
-    if (value) {
-      if(typeof value == "string"){
-        //lets navigate to the href
-        window.location = value;
-      }else if(typeof value == "function"){
-        //it's a function. let's execute it!
-        value();
+  $.fn.mapKey.kdown = function(e){
+    //if keyboard nav is enabled
+    if($.fn.mapKey.enabled){
+      //figure out the code of the key pressed
+      var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+      
+      //check the bindings for the current pressed key
+      var value = $.fn.mapKey.bindings.down[key];
+      
+      if (value) {
+        if(typeof value == "string"){
+          //lets navigate to the href
+          window.location = value;
+        }else if(typeof value == "function"){
+          //it's a function. let's execute it!
+          value();
+        }
+        e.preventDefault();
+      }
+      
+      if($.fn.mapKey.bindings.up[key] != undefined){
+        e.preventDefault();
       }
     }
-    
-    e.preventDefault();
+  }
+  $.fn.mapKey.kup = function(e){
+    //if keyboard nav is enabled
+    if($.fn.mapKey.enabled){  
+      //figure out the code of the key pressed
+      
+      var key = e.charCode ? e.charCode : e.keyCode ? e.keyCode : 0;
+      
+      //check the bindings for the current pressed key
+      var value = $.fn.mapKey.bindings.up[key];
+      
+      if (value) {
+        if(typeof value == "string"){
+          //lets navigate to the href
+          window.location = value;
+        }else if(typeof value == "function"){
+          //it's a function. let's execute it!
+          value();
+        }
+        e.preventDefault();
+      } 
+    }
+  }
+  $.fn.mapKey.enable = function(){
+    //enable mapKey
+    $.fn.mapKey.enabled = true;
+  }
+  $.fn.mapKey.disable = function(){
+    //disable mapKey
+    $.fn.mapKey.enabled = false;
   }
   //
   // plugin defaults
   //
-  $.fn.mapKey.defaults = {};
+  $.fn.mapKey.defaults = {
+    trigger: undefined, 
+    direction: "up"
+  };
   //
   // public variables
   //
-  $.fn.mapKey.bindings = {};
-  
+  $.fn.mapKey.enabled = true;
+  $.fn.mapKey.bindings = { up: {}, down: {}};
   $.fn.mapKey.keys = {
     "back": 8,
     "tab": 9,
@@ -117,16 +181,26 @@ Enjoy!
     "down": 40,
     "insert": 45,
     "del": 46,
-    "0": 48,
-    "1": 49,
-    "2": 50,
-    "3": 51,
-    "4": 52,
-    "5": 53,
-    "6": 54,
-    "7": 55,
-    "8": 56,
-    "9": 57,
+    "top0": 48,
+    "top1": 49,
+    "top2": 50,
+    "top3": 51,
+    "top4": 52,
+    "top5": 53,
+    "top6": 54,
+    "top7": 55,
+    "top8": 56,
+    "top9": 57,
+    "0": [48, 96],
+    "1": [49, 97],
+    "2": [50, 98],
+    "3": [51, 99],
+    "4": [52, 100],
+    "5": [53, 101],
+    "6": [54, 102],
+    "7": [55, 103],
+    "8": [56, 104],
+    "9": [57, 105], 
     "a": 65,
     "b": 66,
     "c": 67,
@@ -155,10 +229,10 @@ Enjoy!
     "z": 90,
     "lwindows": 91,
     "rwindows": 92,
-    "windows": 91,
+    "windows": [91,92],
     "lcmd": 91,
     "rcmd": 92,
-    "cmd": 91,
+    "cmd": [91,92],
     "select": 93,
     "num0": 96,
     "num1": 97,
@@ -203,6 +277,6 @@ Enjoy!
   };
   
   //bind event listeners
-  $(document).bind("keyup", $.fn.mapKey.kpress);
+  $(document).bind("keyup", $.fn.mapKey.kup);
   $(document).bind("keydown", $.fn.mapKey.kdown);
 })(jQuery);
